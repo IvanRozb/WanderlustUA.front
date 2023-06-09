799f0539ee6fe1084aeb5b57c3ffda53a39ae129
IvanRozb <ivanrozb4225@gmail.com>
2023-06-09 18:10:46 +0300

    Added Log In Button

--- src/modules/header/index.tsx
+++ src/modules/header/index.tsx
@@ -1,15 +1,17 @@
 import {AppBar, Container, Toolbar} from "@mui/material";
 import Logo from "@/modules/header/components/logo";
 import NavTabs from "@/modules/header/components/navTabs";
+import Login from "src/modules/header/components/login";
 
 
 export default function Header() {
     return (
         <AppBar position={"static"}>
             <Container maxWidth={"xl"}>
-                <Toolbar disableGutters>
+                <Toolbar disableGutters sx={{display: "flex"}}>
                     <Logo />
                     <NavTabs/>
+                    <Login/>
                 </Toolbar>
             </Container>
         </AppBar>
