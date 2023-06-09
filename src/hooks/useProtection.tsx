ef38fcc0b1ccbbe54ab47fcdf497a3a60a7e11c6
IvanRozb <ivanrozb4225@gmail.com>
2023-06-10 01:49:14 +0300

    Created useProtection for auth routes

---
+++ src/hooks/useProtection.tsx
@@ -0,0 +1,20 @@
+import {useEffect, useState} from "react";
+import {useRouter} from "next/router";
+import useToken from "@/hooks/useToken";
+
+export default function useProtection(){
+    const [isLoading, setLoading] = useState<boolean>(true);
+    const router = useRouter();
+    const token = useToken();
+
+    useEffect(() => {
+        if(token){
+            router.replace("/");
+        }
+        setLoading(false);
+    }, [router])
+
+    if(isLoading){
+        return <p>Loading...</p>
+    }
+}
