c06aa684bce00dcf14eb47162963e2773e089b00
IvanRozb <ivanrozb4225@gmail.com>
2023-06-10 02:53:41 +0300

    Added sign-out functionality

--- src/modules/header/components/sign-in/index.tsx
+++ src/modules/header/components/sign-in/index.tsx
@@ -1,10 +1,8 @@
 import Link from "next/link";
 import {Button, Typography} from "@mui/material";
-import useToken from "@/hooks/useToken";
 
 export default function SignIn(){
-    const token = useToken();
-    return !token && <Button variant="contained" color={"secondary"}>
+    return <Button variant="contained" color={"secondary"}>
         <Link href={"/sign-in"}>
             <Typography variant={"h4"} sx={{cursor: "pointer"}}>Sign In</Typography>
         </Link>
--- src/modules/header/index.tsx
+++ src/modules/header/index.tsx
@@ -2,15 +2,19 @@ import {AppBar, Container, Toolbar} from "@mui/material";
 import Logo from "@/modules/header/components/logo";
 import NavTabs from "@/modules/header/components/navTabs";
 import SignIn from "src/modules/header/components/sign-in";
+import SignOut from "@/modules/header/components/sign-out";
+import useToken from "@/hooks/useToken";
 
 export default function Header() {
+    const token = useToken();
+
     return (
         <AppBar position={"static"}>
             <Container maxWidth={"xl"}>
                 <Toolbar disableGutters>
                     <Logo />
                     <NavTabs/>
-                    <SignIn/>
+                    {!token ? <SignIn/> : <SignOut/>}
                 </Toolbar>
             </Container>
         </AppBar>
