b736bddf9e594418f56ac2bfbc28344449486744
IvanRozb <ivanrozb4225@gmail.com>
2023-06-10 02:15:25 +0300

    Added protection to TripsPage

--- src/pages/trips.tsx
+++ src/pages/trips.tsx
@@ -1,4 +1,10 @@
+import useProtection from "@/hooks/useProtection";
+
 export default function TripsPage() {
+    const res = useProtection();
+    if(res)
+        return res;
+
     return (
        <></>
     )
