0b01920eb1865506bcdf57be9a5df4ef29930e47
IvanRozb <ivanrozb4225@gmail.com>
2023-06-09 17:44:03 +0300

    Tabs is linkable

--- src/modules/header/components/navTabs/index.tsx
+++ src/modules/header/components/navTabs/index.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import {Tab, Tabs} from "@mui/material";
 import {SyntheticEvent, useState} from "react";
 import {makeStyles} from "@mui/styles";
+import theme from "@/helpers/theme";
 
 const useStyles = makeStyles({
     tabs: {
@@ -10,29 +11,28 @@ const useStyles = makeStyles({
         marginLeft: "1rem"
     },
     tab: {
-        textAlign: "center",
         width: "33% !important",
         color: "#fff !important",
         cursor: "pointer",
+    },
+    tabLink: {
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        width: "100%",
+        height: "100%",
     }
 })
 
-export default function NavTabs(){
+export default function NavTabs() {
     const classes = useStyles()
     const [value, setValue] = useState(0);
-
     const handleChange = (event: SyntheticEvent, newValue: number) => {
         setValue(newValue);
     };
     return <Tabs variant={"fullWidth"} value={value} onChange={handleChange} className={classes.tabs}>
-        <Link href={"/"}>
-            <Tab label={"Home"} value={0} className={classes.tab}/>
-        </Link>
-        <Link href={"/tourist-places"}>
-            <Tab label={"Tourist Places"} value={1} className={classes.tab}/>
-        </Link>
-        <Link  href={"/trips"}>
-            <Tab label={"Trips"} value={2} className={classes.tab}/>
-        </Link>
+        <Tab label={<Link href={"/"}><div className={classes.tabLink}>Home</div></Link>} value={0} className={classes.tab}/>
+        <Tab label={<Link href={"/tourist-places"}><div className={classes.tabLink}>Tourist Places</div></Link>} value={1} className={classes.tab}/>
+        <Tab label={<Link href={"/trips"}><div className={classes.tabLink}>Trips</div></Link>} value={2} className={classes.tab}/>
     </Tabs>
 }
